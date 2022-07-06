8b43c088b94f46f51bd16d5de5c18f15c1d06501
luiz gabriel <luizgrfrei@gmail.com>
2022-07-06 12:00:46 -0300

    projeto p-tech sem o react

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+listPokemons();
+
+function listPokemons() {
+    //https://6283929f92a6a5e462260498.mockapi.io/pokemon
+    fetch("https://6283929f92a6a5e462260498.mockapi.io/pokemon")
+        .then(response => response.json())
+        .then(data => {
+            const pokemons = data.map((item) => getContentCard(item));
+
+            document.querySelector(".card__container").innerHTML = pokemons.join('');
+        });
+}
+
+function getContentCard(data) {
+    return `
+    <a id="${data.id}" class="card" href="/${data.id}.html" style="background-color:${data.color || 'red'}">
+      <img class="card__image" src="/assets/images/stat/stat-${data.avatar}.png" alt="${data.name}">
+      <div class="card__title"> ${data.name}</div>
+    </a>
+    `;
+}
